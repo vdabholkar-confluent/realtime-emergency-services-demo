d28ce2f39a8d0b65ea8c842409b5f52c509a53ae
Vishwajeet Dabholkar Confluent <vdabholkar@confluent.io>
2025-08-01 09:56:42 +0530

    Add files via upload

---
+++ UDF/flink-udf-haversine/src/main/java/com/example/udf/HaversineDistance.java
@@ -0,0 +1,17 @@
+package com.example.udf;
+
+import org.apache.flink.table.functions.ScalarFunction;
+
+public class HaversineDistance extends ScalarFunction {
+    public double eval(double lat1, double lon1, double lat2, double lon2) {
+        double R = 6371.0; // Earth radius in kilometers
+        double dLat = Math.toRadians(lat2 - lat1);
+        double dLon = Math.toRadians(lon2 - lon1);
+        double a =
+            Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+            Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2)) *
+            Math.sin(dLon / 2) * Math.sin(dLon / 2);
+        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+        return R * c;
+    }
+}
